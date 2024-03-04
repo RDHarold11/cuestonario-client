1da0f3ad7ff269a806421aab8c7930d6a2ff39a6
Harold Manuel <aquinoharold460@gmail.com>
2024-03-03 22:24:35 -0400

    contants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const RESULTS_URL = "/api/result";
-export const QUESTION_URL = "/api/question";
+export const USERS_URL = "https://cuestonario.onrender.com/api/users";
+export const RESULTS_URL = "https://cuestonario.onrender.com/api/result";
+export const QUESTION_URL = "https://cuestonario.onrender.com/api/question";
